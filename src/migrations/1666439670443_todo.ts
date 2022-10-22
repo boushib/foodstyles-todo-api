dfe936a7ba932d98d0e27cb0a5d1f639f399a26c
El Hassane Boushib <boushib01@gmail.com>
2022-10-22 13:24:53 +0100

    Update todos migration to include user id

--- src/migrations/1666439670443_todo.ts
+++ src/migrations/1666439670443_todo.ts
@@ -11,6 +11,7 @@ export const up = async (pgm: MigrationBuilder): Promise<void> => {
       id SERIAL PRIMARY KEY,
       title VARCHAR(100) NOT NULL,
       status TodoItemStatus DEFAULT 'IN_PROGRESS',
+      user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
