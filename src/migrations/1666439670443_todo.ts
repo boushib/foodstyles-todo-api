8a31ed8ce1a05971fc3f84e52957b063024609c9
El Hassane Boushib <boushib01@gmail.com>
2022-10-22 13:15:07 +0100

    Create todo migration

---
+++ src/migrations/1666439670443_todo.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
+
+export const shorthands: ColumnDefinitions | undefined = undefined
+
+export const up = async (pgm: MigrationBuilder): Promise<void> => {
+  pgm.sql(`
+    CREATE TYPE TodoItemStatus AS ENUM ('IN_PROGRESS', 'COMPLETED');
+
+    CREATE TABLE todos (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(100) NOT NULL,
+      status TodoItemStatus DEFAULT 'IN_PROGRESS',
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `)
+}
+
+export const down = async (pgm: MigrationBuilder): Promise<void> => {
+  pgm.sql(`
+    DROP TABLE todos;
+    DROP TYPE TodoItemStatus;
+  `)
+}
